eca31d7287c95c9d2a4091a17e490f471f591f20
AsbarAli <loons.create@gmail.com>
2019-06-03 12:10:37 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {CustomCounterTimerContainer} from './src/containers/';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => CustomCounterTimerContainer);
