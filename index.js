56f304f40dc581b38f45cd6f1b32fc9b8c6f6720
AsbarAli <loons.create@gmail.com>
2019-06-03 13:08:04 +0530

    Remove app json

--- index.js
+++ index.js
@@ -4,6 +4,5 @@
 
 import {AppRegistry} from 'react-native';
 import {CustomCounterTimerContainer} from './src/containers/';
-import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => CustomCounterTimerContainer);
